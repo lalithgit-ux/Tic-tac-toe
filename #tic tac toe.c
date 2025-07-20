3733024c6b05c82992de870b9d61e38798311232
lalithgit-ux <dontamsettylalithmanoj@gmail.com>
2025-07-20 14:34:56 +0530

    Update and rename #tic tac toe to #tic tac toe.c

--- #tic tac toe.c
+++ #tic tac toe.c
@@ -1,5 +1,5 @@
-# Tic-tac-toe
-#This code is regarding tic tac toe game which it i made by using c language and also it is used for making your 1st year project. and also it is super exciting to play.
+//Tic-tac-toe
+//This code is regarding tic tac toe game which it i made by using c language and also it is used for making your 1st year project. and also it is super exciting to play.
 
 #include <stdio.h>
 char board[3][3];
